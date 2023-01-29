2993c27e669ffbccf7d517a4326cd50c6ca19400
Ryebread5555 <Rypetty55@gmail.com>
2023-01-29 12:24:38 -0600

    created markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -14,6 +15,42 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.licenses)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Questions regarding this project?
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
+
+  ## Credits
+  ${data.name}
 `;
 }
 
